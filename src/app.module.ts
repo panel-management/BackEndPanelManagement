4c53278b4f330bb845f8cf1c6ee04c11e916dd13
Ehsan-Fouladi <fooehan3@gmail.com>
2025-08-06 23:23:01 +0330

    added module auth and user

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, UsersModule, AuthModule],
 })
 export class AppModule {}

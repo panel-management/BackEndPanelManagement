bf141939c22898fe05af272c8783794c807ba543
Ehsan-Fouladi <fooehan3@gmail.com>
2025-08-07 13:18:36 +0330

    create users and set role

---
+++ src/auth/enums/role.enum.ts
@@ -0,0 +1,5 @@
+export enum Role {
+  Admin = 0,
+  Master = 1,
+  Student = 2,
+}
---
+++ src/auth/guards/roles.guard.ts
@@ -0,0 +1,46 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { ROLES_KEY } from '../decorators/roles.decorator';
+import { UserPayload } from '../interfaces/user-payload.interface';
+import { Role } from '../enums/role.enum';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+  canActivate(context: ExecutionContext): boolean {
+    const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (!requiredRoles) {
+      return true;
+    }
+
+    const request = context.switchToHttp().getRequest();
+    const user: UserPayload = request.user;
+
+    if (!user || user.type === undefined) {
+      throw new ForbiddenException({
+        statusCode: 403,
+        message: 'شما دسترسی لازم برای این بخش را ندارید.',
+      });
+    }
+
+    const hasRole = () => requiredRoles.some((role) => user.type == role);
+
+    if (hasRole()) {
+      return true;
+    }
+
+    throw new ForbiddenException({
+      statusCode: 403,
+      message: 'شما دسترسی لازم برای این بخش را ندارید.',
+    });
+  }
+}

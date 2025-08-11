a779e0bc177f99137f93e0f1250fc106aa02917e
Ehsan-Fouladi <fooehan3@gmail.com>
2025-08-11 12:49:03 +0330

    added module sportandbelt

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,11 +3,12 @@ import { UsersService } from './users.service';
 import { StudentService } from './student/student.service';
 import { StudentController } from './student/student.controller';
 import { StudentModule } from './student/student.module';
+import { SportBeltModule } from './sport-belt/sport-belt.module';
 
 @Module({
   providers: [UsersService, StudentService],
   exports: [UsersService],
   controllers: [StudentController],
-  imports: [StudentModule],
+  imports: [StudentModule, SportBeltModule],
 })
 export class UsersModule {}

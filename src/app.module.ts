bc1616942e2507de39110aec5f439fe02f9c3849
Ehsan-Fouladi <fooehan3@gmail.com>
2025-08-13 06:47:09 +0330

    config multer path image

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,12 +6,18 @@ import { ConfigModule } from '@nestjs/config';
 import jwtConfig from './auth/config/jwt.config';
 import { APP_GUARD } from '@nestjs/core';
 import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       load: [jwtConfig],
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(process.cwd(), 'uploads'),
+      serveRoot: '/uploads',
     }),
     PrismaModule,
     UsersModule,

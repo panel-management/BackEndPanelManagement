63d9dddfb531a8726836e6a2ede06ca733742a0c
Ehsan-Fouladi <fooehan3@gmail.com>
2025-08-19 10:50:44 +0330

    set versioning and added api router and core

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api');
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -15,6 +22,8 @@ async function bootstrap() {
       },
     }),
   );
+
+  app.enableCors();
 
   await app.listen(process.env.PORT ?? 3000);
 }

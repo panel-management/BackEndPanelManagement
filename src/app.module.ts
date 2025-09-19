202efd1de420f7f2f0ebba851453cfec28f07196
Ehsan-Fouladi <fooehan3@gmail.com>
2025-09-19 11:52:15 +0330

    change code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';

1392bd16c480366e507432b8e93a13ef04089d9e
Ehsan-Fouladi <fooehan3@gmail.com>
2026-04-27 00:27:22 +0330

    install swagger and axios and setup swagger

--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,17 @@ import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import { AllExceptionFilter } from './common/filters/all-exception.filter';
 import { LoggerService } from './common/logger/logger.service';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { DocumentBuilder, SwaggerCustomOptions, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const isProd = process.env.NODE_ENV === 'production';
+
+  const config = new DocumentBuilder()
+    .setTitle('club')
+    .setDescription('panel management club')
+    .setVersion('1.0.0')
+    .build()
 
   app.use(
     helmet({
@@ -52,6 +59,17 @@ async function bootstrap() {
       },
     }),
   );
+
+  const configSwagger: SwaggerCustomOptions = {
+    useGlobalPrefix: true,
+    explorer: true,
+    swaggerOptions: {
+      persistAuthorization: true
+    }
+  }
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/', app, documentFactory, configSwagger);
 
   await app.listen(3000);
 }

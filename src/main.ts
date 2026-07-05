62b3c74066f68662a31e4d9394209ad3a9c80edf
Ehsan-Fouladi <fooehan3@gmail.com>
2026-07-05 13:35:27 +0330

    refacktor docker file and docker compose valume and url docsument swagger

--- src/main.ts
+++ src/main.ts
@@ -69,7 +69,7 @@ async function bootstrap() {
   };
 
   const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('/', app, documentFactory, configSwagger);
+  SwaggerModule.setup(`/docs`, app, documentFactory, configSwagger);
 
   await app.listen(3000);
 }

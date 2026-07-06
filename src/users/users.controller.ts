bf2b6248a55d73db0eb02c729f1ac2e7eecfa869
Ehsan-Fouladi <fooehan3@gmail.com>
2026-07-06 13:32:20 +0330

    writhing docuemnt users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -4,14 +4,19 @@ import { RolesGuard } from 'src/auth/guards/roles.guard';
 import { Roles } from 'src/auth/decorators/roles.decorator';
 import { Role } from 'src/auth/enums/role.enum';
 import { UsersService } from './users.service';
+import { ApiBearerAuth, ApiOkResponse, ApiOperation } from '@nestjs/swagger';
 
 @Controller('users')
+@ApiBearerAuth("authorization")
 @UseGuards(JwtAuthGuard, RolesGuard)
+@Roles(Role.Admin, Role.Master, Role.Coach, Role.Student)
 export class UsersController {
   constructor(private readonly users: UsersService) {}
 
   // get data user
   @Get()
+  @ApiOperation({ summary: "نمایش پروفایل کاربر" })
+  @ApiOkResponse({ description: "پروفایل کاربر با موفقیت دریافت شد" })
   @HttpCode(HttpStatus.OK)
   getAllProfileUser(@Req() req) {
     return this.users.getProfileUsers(req.user.userId);
@@ -19,7 +24,8 @@ export class UsersController {
 
   // get plan status
   @Get('plan/status')
-  @Roles(Role.Admin, Role.Master, Role.Coach, Role.Student)
+  @ApiOperation({ summary: "نمایش وضعیت پلن کاربر" })
+  @ApiOkResponse({ description: "پلن با موفقیت دریافت شد" })
   @HttpCode(HttpStatus.OK)
   getMyPlanStatus(@Req() req) {
     return this.users.getPlanStatus(req.user.userId);

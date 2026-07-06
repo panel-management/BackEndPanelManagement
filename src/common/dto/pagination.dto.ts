aab83d595826a58756890f91bbc2e85f4fb7f501
Ehsan-Fouladi <fooehan3@gmail.com>
2026-07-06 23:35:32 +0330

    writhe documenting pagination and update status

--- src/common/dto/pagination.dto.ts
+++ src/common/dto/pagination.dto.ts
@@ -1,13 +1,25 @@
+import { ApiPropertyOptional } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
 import { IsInt, IsOptional, Max, Min } from 'class-validator';
 
 export class PaginationQueryDto {
+  @ApiPropertyOptional({
+    example: 1,
+    minimum: 1,
+    description: 'شماره صفحه',
+  })
   @IsOptional()
   @Type(() => Number)
   @IsInt()
   @Min(1)
   page?: number;
 
+  @ApiPropertyOptional({
+    example: 10,
+    minimum: 1,
+    maximum: 100,
+    description: 'تعداد آیتم در هر صفحه',
+  })
   @IsOptional()
   @Type(() => Number)
   @IsInt()
--- src/common/dto/updateStatus.dto.ts
+++ src/common/dto/updateStatus.dto.ts
@@ -1,6 +1,11 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsBoolean, IsNotEmpty } from 'class-validator';
 
 export class UpdateStatusDto {
+  @ApiProperty({
+    example: true,
+    description: 'وضعیت فعال یا غیرفعال بودن',
+  })
   @IsBoolean({ message: 'وضعیت با به صورت true یا false باشد' })
   @IsNotEmpty({ message: 'وضعیت نمی‌ تواند خالی باشد' })
   isActive: boolean;

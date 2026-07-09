84075e73df6ee943b68631ffe484cb26144a55ef
Ehsan-Fouladi <fooehan3@gmail.com>
2026-07-09 12:54:33 +0330

    edit dto for update

--- src/users/coach/dto/update-coach.dto.ts
+++ src/users/coach/dto/update-coach.dto.ts
@@ -1,39 +1,4 @@
 import { PartialType } from "@nestjs/swagger";
-// import { Type } from 'class-transformer';
-// import { IsDate, IsInt, IsOptional, IsPhoneNumber, IsString, Matches } from 'class-validator';
 import { CreateCoachDto } from "./create-coach.dto";
 
 export class UpdateCoachDto extends PartialType(CreateCoachDto) {}
-
-// export class UpdateCoachDto {
-//   @IsString()
-//   @IsOptional()
-//   fullName?: string;
-
-//   @IsString()
-//   @IsOptional()
-//   nationalCode?: string;
-
-//   @IsOptional()
-//   @IsPhoneNumber("IR", { message: "شماره تلفن معتبر وارد کنید" })
-//   @Matches(/^09\d{9}$/, { message: 'فرمت شماره تلفن نامعتبر است' })
-//   phoneNumber?: string;
-
-//   @IsDate()
-//   @IsOptional()
-//   @Type(() => Date)
-//   birthDate?: Date;
-
-//   @IsInt()
-//   @IsOptional()
-//   @Type(() => Number)
-//   age?: number;
-
-//   @IsString()
-//   @IsOptional()
-//   history?: string;
-
-//   @IsString()
-//   @IsOptional()
-//   certificates?: string;
-// }
